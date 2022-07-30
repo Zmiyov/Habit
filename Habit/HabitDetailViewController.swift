327063399839096b0877699ab72bfd20fd8e7f6c
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-07-30 19:19:24 +0300

    Created main view controllers

---
+++ Habit/HabitDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HabitDetailViewController.swift
+//  Habit
+//
+//  Created by Vladimir Pisarenko on 30.07.2022.
+//
+
+import UIKit
+
+class HabitDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
