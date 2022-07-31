9bc7e519abf95b01b66992d558b5be43662cda9b
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-07-31 12:01:53 +0300

    Add network code

--- Habit/Habit.swift
+++ Habit/Habit.swift
@@ -35,3 +35,23 @@ extension Color: Codable {
 
 extension Category: Codable { }
 extension Habit: Codable { }
+
+extension Habit: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
+    static func == (lhs: Habit, rhs: Habit) -> Bool {
+        return lhs.name == rhs.name
+    }
+}
+
+extension Category: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+    
+    static func == (lhs: Category, rhs: Category) -> Bool {
+        lhs.name == rhs.name
+    }
+}
---
+++ Habit/Settings.swift
@@ -0,0 +1,42 @@
+//
+//  Settings.swift
+//  Habit
+//
+//  Created by Vladimir Pisarenko on 31.07.2022.
+//
+
+import Foundation
+
+struct Settings {
+    static var shared = Settings()
+    private let defaults = UserDefaults.standard
+    
+    var favoriteHabits: [Habit] {
+        get {
+            return unarchiveJSON(key: Setting.favoriteHabits) ?? []
+        }
+        set {
+            archiveJSON(value: newValue, key: Setting.favoriteHabits)
+        }
+    }
+    
+    enum Setting {
+        static let favoriteHabits = "favoriteHabits"
+    }
+    
+    private func archiveJSON<T: Encodable>(value: T, key: String) {
+        let data = try! JSONEncoder().encode(value)
+        let string = String(data: data, encoding: .utf8)
+        defaults.set(string, forKey: key)
+    }
+    
+    private func unarchiveJSON<T: Decodable>(key: String) -> T? {
+        guard let string = defaults.string(forKey: key),
+              let data = string.data(using: .utf8) else {
+            return nil
+        }
+        return try! JSONDecoder().decode(T.self, from: data)
+    }
+    
+    
+}
