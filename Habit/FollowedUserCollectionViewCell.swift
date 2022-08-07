255535d9b715a68fbd61f73eef3264b6132fffe3
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-08-07 15:02:18 +0300

    FollowedUserCell

---
+++ Habit/FollowedUserCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  FollowedUserCollectionViewCell.swift
+//  Habit
+//
+//  Created by Vladimir Pisarenko on 07.08.2022.
+//
+
+import UIKit
+
+class FollowedUserCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var primaryTextLabel: UILabel!
+    @IBOutlet var secondaryTextLabel: UILabel!
+    
+}
