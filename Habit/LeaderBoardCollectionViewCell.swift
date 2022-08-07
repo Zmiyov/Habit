ff1ac2cca2ff82f9aeb46c9757fbcd24990330da
Volodymyr Pysarenko <zmiyov88@gmail.com>
2022-08-07 14:41:01 +0300

    Setup Home View Controller scene

---
+++ Habit/LeaderBoardCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  LeaderBoardCollectionViewCell.swift
+//  Habit
+//
+//  Created by Vladimir Pisarenko on 07.08.2022.
+//
+
+import UIKit
+
+class LeaderBoardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var habitNameLabel: UILabel!
+    @IBOutlet var leaderLabel: UILabel!
+    @IBOutlet var secondaryLabel: UILabel!
+    
+    
+    
+}
